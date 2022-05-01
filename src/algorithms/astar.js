efdcd1a2648e82e5d0da2c09fd171c190c81bcfd
Gerardo <gr261212@gmail.com>
2022-04-30 20:27:41 -0400

    update astar to use Euclidean distance

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -1,86 +1,43 @@
 // manhattan heuristics
-function heuristic(neighbor, endNode) {
+function heuristic(node, endNode) {
     // let D = 0;
-    let dx = Math.abs(neighbor.row - endNode.row);
-    let dy = Math.abs(neighbor.col - endNode.col);
-    return dx + dy; //Math.sqrt(dx * dx + dy * dy);
+    let dx = Math.abs(node.row - endNode.row);
+    let dy = Math.abs(node.col - endNode.col);
+    // return dx + dy; //Math.sqrt(dx * dx + dy * dy);
+    return Math.sqrt(
+        Math.pow(node.row - endNode.row, 2) +
+            Math.pow(node.col - endNode.col, 2),
+    );
 }
 
-const updateUnvisitedNeighbors = (
-    currentNode,
-    endNode,
-    grid,
-    openList,
-    closedList,
-) => {
+const updateUnvisitedNeighbors = (currentNode, grid, endNode, openList) => {
     let neighbors = getNeighbors(grid, currentNode);
     for (let neighbor of neighbors) {
-        if (neighbor.closed || neighbor.isWall) continue;
-
         // tentative gscore
-        let gScore =
-            currentNode.g +
-            (neighbor.row - currentNode.row === 0 ||
-            neighbor.col - currentNode.col === 0
-                ? 1
-                : Math.SQRT2);
-
+        let gScore = currentNode.g + 1;
         // find the best current path
-        if (!neighbor.isVisited || gScore < neighbor.g) {
+        if (gScore < neighbor.g) {
             neighbor.previousNode = currentNode;
             neighbor.g = gScore;
-            neighbor.h = neighbor.h || 1 * heuristic(neighbor, endNode);
+            neighbor.h = heuristic(neighbor, endNode);
             neighbor.f = neighbor.g + neighbor.h;
             neighbor.distance = currentNode.distance + 1;
-
-            if (!neighbor.isVisited) {
+            if (!openList.includes(neighbor)) {
                 openList.push(neighbor);
-                neighbor.isVisited = true;
-            } else {
-                openList.map((node) => {
-                    if (
-                        node.row === neighbor.row &&
-                        node.col === neighbor.col
-                    ) {
-                        let a = {
-                            ...node,
-                            ...neighbor,
-                        };
-                        return a;
-                    }
-                    return node;
-                });
             }
         }
     }
 };
 
 //neighbors of currentNode
-function getNeighbors(grid, node, diagonals = false) {
+function getNeighbors(grid, node) {
     const neighbors = [];
     const { col, row } = node;
     if (row > 0) neighbors.push(grid[row - 1][col]);
     if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
     if (col > 0) neighbors.push(grid[row][col - 1]);
     if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-    if (diagonals) {
-        // Southwest
-        if (grid[row - 1] && grid[row - 1][col - 1]) {
-            neighbors.push(grid[row - 1][col - 1]);
-        }
-        // Southeast
-        if (grid[row + 1] && grid[row + 1][col - 1]) {
-            neighbors.push(grid[row + 1][col - 1]);
-        }
-        // Northwest
-        if (grid[row - 1] && grid[row - 1][col + 1]) {
-            neighbors.push(grid[row - 1][col + 1]);
-        }
-        // Northeast
-        if (grid[row + 1] && grid[row + 1][col + 1]) {
-            neighbors.push(grid[row + 1][col + 1]);
-        }
-    }
+
     return neighbors.filter((neighbor) => !neighbor.isVisited);
 }
 
@@ -95,47 +52,46 @@ export const astar = (grid, startNode, endNode) => {
     // set initial properties of start node
     startNode.distance = 0;
     startNode.g = 0;
-    startNode.f = 0;
-
+    startNode.h = heuristic(startNode, endNode);
+    startNode.f = startNode.h;
     // add start node to nodes that have been explored
     openList.push(startNode);
-    startNode.isVisited = true;
 
     while (openList.length !== 0) {
         // sort list in order of min to max fscore
         sortList(openList);
         // get the node with the lowest fscore
         let currentNode = openList.shift();
-        currentNode.closed = true;
+        if (currentNode.isWall) continue;
+        // currentNode.closed = true;
+        currentNode.isVisited = true;
 
         // if no path return the explored nodes
-        if (currentNode.distance === Infinity) return closedList;
+        if (currentNode.distance === Infinity) {
+            return closedList;
+        }
 
         // if end goal is reached, return explored nodes
-        if (currentNode === endNode) return closedList;
-
-        // add current node to explored nodes
+        if (currentNode === endNode) {
+            return closedList;
+        }
         if (!currentNode.startNode) closedList.push(currentNode);
 
         // update the neighbors of our current node
-        updateUnvisitedNeighbors(
-            currentNode,
-            endNode,
-            grid,
-            openList,
-            closedList,
-        );
+        updateUnvisitedNeighbors(currentNode, grid, endNode, openList);
     }
+    return closedList;
 };
 
 // get the nodes in the shortest path
 export const getNodesInShortestPathOrderAStar = (finishNode) => {
     const shortestPathOrder = [];
     let currentNode = finishNode.previousNode;
-    while (currentNode !== null && currentNode.isVisited) {
+    while (!currentNode.startNode) {
         shortestPathOrder.unshift(currentNode);
         currentNode = currentNode.previousNode;
     }
 
+    console.log(shortestPathOrder);
     return shortestPathOrder;
 };
