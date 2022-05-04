8c844ffb375c5da708c3a375a04beb55e69ba164
Gerardo <gr261212@gmail.com>
2022-05-04 09:55:13 -0400

    update style and markup

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,15 +2,12 @@ import React, { useEffect, useRef } from "react";
 import "../styles/Home.css";
 import Transition from "../components/Transition";
 import { gsap } from "gsap";
-
 export default function Main() {
     const main = gsap.timeline();
     const mainh1 = useRef(null);
     const mainimg = useRef(null);
 
     useEffect(() => {
-        // main.from(mainh1.current, 1, {height: "0%"}, {height: '80%', ease: Power2.easeInOut})
-        // main.from(mainimg.current, 1.2, {width:'100%'}, {width:'80%', ease: Power2.easeInOut})
         main.from(
             mainh1.current,
             {
@@ -30,15 +27,15 @@ export default function Main() {
             },
             "-=3",
         );
-    });
+    }, [main]);
 
     return (
-        <div>
+        <>
             <Transition timeline={main} />
-            <div className="main-image main-overlay" ref={mainimg}></div>
             <div className="container-main">
+                <div className="main-image main-overlay" ref={mainimg} />
                 <h1 ref={mainh1}>See the Path</h1>
             </div>
-        </div>
+        </>
     );
 }
