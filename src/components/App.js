bf05886243e4fd10cf0d0d75912531525d48561b
Gerardo <gr261212@gmail.com>
2022-05-05 23:29:07 -0400

    add routes to algo pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { GridProvider } from "../context/gridContext";
 import Layout from "./Layout";
-import { Home, Algo, Visualizer } from "../Pages";
+import {
+    Home,
+    Algo,
+    Visualizer,
+    DijkstraPage,
+    AStarPage,
+    BFSPage,
+    DFSPage,
+} from "../Pages";
 
 function App() {
     return (
@@ -11,8 +19,13 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="/algo" element={<Algo />} />
                     <Route path="/visualizer" element={<Visualizer />} />
+                    <Route path="/algo" element={<Algo />}>
+                        <Route index element={<DijkstraPage />} />
+                        <Route path="DFSPage" element={<DFSPage />} />
+                        <Route path="BFSPage" element={<BFSPage />} />
+                        <Route path="AStarPage" element={<AStarPage />} />
+                    </Route>
                 </Route>
             </Routes>
         </GridProvider>
